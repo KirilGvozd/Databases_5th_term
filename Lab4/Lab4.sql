0ec0e49c7991e1f8a5def55d6a707dd1035a86e5
Kirill <kirillgvozd0@gmail.com>
2023-07-16 22:20:01 +0300

    Started the 4th lab, made 3 tasks

---
+++ Lab4/Lab4.sql
@@ -0,0 +1,11 @@
+-- Задание №1
+SELECT name, open_mode FROM v$pdbs;
+SELECT pdb_name, pdb_id, status FROM SYS.dba_pdbs;
+
+-- Задание №2
+SELECT * FROM v$instance;
+
+-- Задание №3
+SELECT * FROM SYS.PRODUCT_COMPONENT_VERSION;
+
+-- Задание №4
